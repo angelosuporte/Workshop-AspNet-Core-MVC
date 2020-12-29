1a2abd22eeb9701e257106bb524777953e76e210
Angelo Carvalho <angelosuporteti@gmail.com>
2020-12-29 12:18:57 -0300

    Simple create form com método para inserir vendedor no BD

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -20,5 +20,15 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        internal void Insert(SellerService seller)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
