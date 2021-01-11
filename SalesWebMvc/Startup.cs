bddb29470b0be9733ef0c98f2b4edc1ea97b16c1
Angelo Carvalho <angelosuporteti@gmail.com>
2021-01-11 12:44:04 -0300

    Validation

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -52,14 +52,14 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
-            var enUS = new CultureInfo("en-US");
+           var enUS = new CultureInfo("en-US");
             var localizationOptions = new RequestLocalizationOptions
             {
                 DefaultRequestCulture = new RequestCulture(enUS),
                 SupportedCultures = new List<CultureInfo> { enUS },
                 SupportedUICultures = new List<CultureInfo> { enUS }
             };
-
+ 
             app.UseRequestLocalization(localizationOptions);
 
             if (env.IsDevelopment())
